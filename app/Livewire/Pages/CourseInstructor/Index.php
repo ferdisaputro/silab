2dc532321ca388d005b4ed21ef518afec8a17978
ferdisaputro <ferdisaputro1@gmail.com>
2025-01-09 02:20:57 +0700

    add course instructor page feature

--- app/Livewire/Pages/CourseInstructor/Index.php
+++ app/Livewire/Pages/CourseInstructor/Index.php
@@ -3,13 +3,25 @@
 namespace App\Livewire\Pages\CourseInstructor;
 
 use App\Models\AcademicYear;
+use App\Models\Course;
 use App\Models\Department;
+use App\Models\Semester;
 use Livewire\Attributes\Computed;
+use Livewire\Attributes\Validate;
 use Livewire\Component;
 
 class Index extends Component
 {
     public $listeners = ['setLecturer', 'setCourse'];
+
+    #[Validate('required|integer|exists:departments,id')]
+    public $departmentId; // id of department table
+    #[Validate('required|integer|exists:study_programs,id')]
+    public $studyProgramId; // id of study program table
+    #[Validate('required|integer|exists:academic_years,id')]
+    public $academicYearId; // id of academic year table
+    #[Validate('required|integer|exists:semesters,id')]
+    public $semesterId; // id of semester table
 
     public $selectedLecturer;
     public $selectedCourse;
@@ -36,13 +48,19 @@ class Index extends Component
         return $academicYear ? $academicYear->semesters()->get() : [];
     }
 
-    public function setCourse($course) {
-        $this->selectedCourse = $course;
+    #[Computed()]
+    public function semesterCourses() {
+        $semester = Semester::find($this->semesterId);
+        return $semester ? $semester->semesterCourses->where('study_program_id', $this->studyProgramId)->load('course') : [];
     }
 
-    public function setLecturer($lecturer) {
-        $this->selectedLecturer = $lecturer;
-    }
+    // public function setCourse($course) {
+    //     $this->selectedCourse = $course;
+    // }
+
+    // public function setLecturer($lecturer) {
+    //     $this->selectedLecturer = $lecturer;
+    // }
 
     public function render()
     {
