cb2f5f7e46eb80260347ad5f520728313ce0cb79
ThanpaNama <satryavian@gmail.com>
2025-01-10 23:40:03 +0700

    new branch adhek

---
+++ database/seeders/course_instructuresSeeder.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class course_instructuresSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        //
+    }
+}
