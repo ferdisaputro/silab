9bea88a548e9e89a007f3f0ab15c60890ccf7e76
developer <@e41230708polije.ac.id>
2025-04-28 08:15:48 +0700

    update frontend pengampu matkul

--- resources/views/livewire/pages/semester-course/index.blade.php
+++ resources/views/livewire/pages/semester-course/index.blade.php
@@ -33,24 +33,24 @@
                     <hr>
                     <!-- Judul Pilih Jurusan -->
                     <h5 class="text-lg font-semibold text-center">Pilih Jurusan</h5>
-                    
+
                     <!-- Daftar Jurusan -->
                     <ul class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-2 text-sm font-medium text-gray-900 bg-white border border-gray-200 rounded-lg p-2 dark:bg-gray-700 dark:border-gray-600 dark:text-white">
                         @foreach ($this->departments as $department)
                             <li class="border border-gray-200 rounded-lg dark:border-gray-600">
                                 <div class="flex items-center ps-3">
-                                    <input 
-                                        id="department-radio-{{ $department->id }}" 
-                                        type="radio" 
-                                        value="{{ $department->id }}" 
-                                        wire:model.live="departmentId" 
-                                        name="departmentId" 
-                                        class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 
-                                            dark:focus:ring-blue-600 dark:ring-offset-gray-700 
-                                            dark:focus:ring-offset-gray-700 focus:ring-2 dark:bg-gray-600 
+                                    <input
+                                        id="department-radio-{{ $department->id }}"
+                                        type="radio"
+                                        value="{{ $department->id }}"
+                                        wire:model.live="departmentId"
+                                        name="departmentId"
+                                        class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500
+                                            dark:focus:ring-blue-600 dark:ring-offset-gray-700
+                                            dark:focus:ring-offset-gray-700 focus:ring-2 dark:bg-gray-600
                                             dark:border-gray-500"
                                     >
-                                    <label for="department-radio-{{ $department->id }}" 
+                                    <label for="department-radio-{{ $department->id }}"
                                         class="w-full py-3 ms-2 text-sm font-medium text-gray-900 dark:text-gray-300">
                                         {{ $department->department }}
                                     </label>
@@ -59,7 +59,7 @@
                         @endforeach
                     </ul>
                 </div>
-                <!-- @dump($this->studyPrograms) -->
+                {{-- <!-- @dump($this->studyPrograms) --> --}}
                 <div class="flex flex-col gap-4">
                     <hr>
                     <!-- Judul Pilih Program Studi -->
@@ -70,18 +70,18 @@
                         @foreach ($this->studyPrograms as $studyProgram)
                             <li class="border border-gray-200 rounded-lg dark:border-gray-600">
                                 <div class="flex items-center ps-3">
-                                    <input 
-                                        id="studyProgram-radio-{{ $studyProgram->id }}" 
-                                        type="radio" 
-                                        value="{{ $studyProgram->id }}" 
-                                        wire:model.live="studyProgramId" 
-                                        name="studyProgramId" 
-                                        class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 
-                                            dark:focus:ring-blue-600 dark:ring-offset-gray-700 
-                                            dark:focus:ring-offset-gray-700 focus:ring-2 dark:bg-gray-600 
+                                    <input
+                                        id="studyProgram-radio-{{ $studyProgram->id }}"
+                                        type="radio"
+                                        value="{{ $studyProgram->id }}"
+                                        wire:model.live="studyProgramId"
+                                        name="studyProgramId"
+                                        class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500
+                                            dark:focus:ring-blue-600 dark:ring-offset-gray-700
+                                            dark:focus:ring-offset-gray-700 focus:ring-2 dark:bg-gray-600
                                             dark:border-gray-500"
                                     >
-                                    <label for="studyProgram-radio-{{ $studyProgram->id }}" 
+                                    <label for="studyProgram-radio-{{ $studyProgram->id }}"
                                         class="w-full py-3 ms-2 text-sm font-medium text-gray-900 dark:text-gray-300">
                                         {{ $studyProgram->study_program }}
                                     </label>
@@ -101,18 +101,18 @@
                         @foreach ($this->academicYears as $academicYear)
                             <li class="border border-gray-200 rounded-lg dark:border-gray-600">
                                 <div class="flex items-center ps-3">
-                                    <input 
-                                        id="academicYear-radio-{{ $academicYear->id }}" 
-                                        type="radio" 
-                                        value="{{ $academicYear->id }}" 
-                                        wire:model.live="academicYearId" 
-                                        name="academicYearId" 
-                                        class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 
-                                            dark:focus:ring-blue-600 dark:ring-offset-gray-700 
-                                            dark:focus:ring-offset-gray-700 focus:ring-2 dark:bg-gray-600 
+                                    <input
+                                        id="academicYear-radio-{{ $academicYear->id }}"
+                                        type="radio"
+                                        value="{{ $academicYear->id }}"
+                                        wire:model.live="academicYearId"
+                                        name="academicYearId"
+                                        class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500
+                                            dark:focus:ring-blue-600 dark:ring-offset-gray-700
+                                            dark:focus:ring-offset-gray-700 focus:ring-2 dark:bg-gray-600
                                             dark:border-gray-500"
                                     >
-                                    <label for="academicYear-radio-{{ $academicYear->id }}" 
+                                    <label for="academicYear-radio-{{ $academicYear->id }}"
                                         class="w-full py-3 ms-2 text-sm font-medium text-gray-900 dark:text-gray-300">
                                         {{ $academicYear->start_year }} / {{ $academicYear->end_year }} ({{ $academicYear->is_even ? 'Genap' : 'Ganjil' }})
                                     </label>
@@ -132,18 +132,18 @@
                         @foreach ($this->semesters as $semester)
                             <li class="border border-gray-200 rounded-lg dark:border-gray-600">
                                 <div class="flex items-center ps-3">
-                                    <input 
-                                        id="semester-radio-{{ $semester->id }}" 
-                                        type="radio" 
-                                        value="{{ $semester->id }}" 
-                                        wire:model.live="semesterId" 
-                                        name="semesterId" 
-                                        class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 
-                                            dark:focus:ring-blue-600 dark:ring-offset-gray-700 
-                                            dark:focus:ring-offset-gray-700 focus:ring-2 dark:bg-gray-600 
+                                    <input
+                                        id="semester-radio-{{ $semester->id }}"
+                                        type="radio"
+                                        value="{{ $semester->id }}"
+                                        wire:model.live="semesterId"
+                                        name="semesterId"
+                                        class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500
+                                            dark:focus:ring-blue-600 dark:ring-offset-gray-700
+                                            dark:focus:ring-offset-gray-700 focus:ring-2 dark:bg-gray-600
                                             dark:border-gray-500"
                                     >
-                                    <label for="semester-radio-{{ $semester->id }}" 
+                                    <label for="semester-radio-{{ $semester->id }}"
                                         class="w-full py-3 ms-2 text-sm font-medium text-gray-900 dark:text-gray-300">
                                         {{ $semester->semester }}
                                     </label>
@@ -154,7 +154,7 @@
                 </div>
 
                 <!-- <hr> -->
-                
+
                 <div class="flex flex-col gap-4">
                     <hr>
                     <x-forms.input wire:model.live.debounce='totalGroup' name="totalGroup" label="Jumlah Golongan" type="number" class="flex-1" />
