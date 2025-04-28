2e7d27c088ec459d9400f877f56722f9e6a3464d
ThanpaNama <satryavian@gmail.com>
2025-04-28 10:48:29 +0700

    serah terima

--- app/Livewire/Pages/HandoverPracticalResult/Index.php
+++ app/Livewire/Pages/HandoverPracticalResult/Index.php
@@ -2,10 +2,40 @@
 
 namespace App\Livewire\Pages\HandoverPracticalResult;
 
+use App\Models\Laboratory;
+use App\Models\PracticumResultHandover;
+use Illuminate\Support\Facades\Auth;
+use Livewire\Attributes\Computed;
 use Livewire\Component;
 
 class Index extends Component
 {
+    public $handOverPerPage = 15;
+    public $handOverFilter = null;
+    public $handOverOrderBy = 'id';
+    public $handOverOrderByDirection = 'asc';
+    public $selectedLab;
+
+    // #[Computed()]
+    // public function handOvers() {
+    //     return PracticumResultHandover::when($this->handOverOrderBy && $this->handOverOrderByDirection, function ($query) {
+    //                     $query->orderBy($this->handOverOrderBy, $this->handOverOrderByDirection);
+    //                 })
+    //                 ->where('laboratory_id', $this->selectedLab)
+    //                 // ->with('staffBorrower', 'staffBorrower.user')
+    //                 ->paginate($this->handOverPerPage);
+    //                 // where('columnName', 'like', "%$this->equipmentLoanFilter%")
+    //                 // ->orderBy($this->equipmentLoanOrderBy, $this->equipmentLoanOrderByDirection)
+    // }
+
+    #[Computed()]
+    public function laboratories() {
+        return Laboratory::whereIn('id', Auth::user()->labMembers->pluck('laboratory_id'))->get();
+    }
+
+    public function mount(){
+        $this->selectedLab = $this->laboratories()->first()? $this->laboratories()->first()->id : null;
+    }
     public function render()
     {
         return view('livewire.pages.handover-practical-result.index');
--- resources/views/livewire/pages/handover-practical-result/index.blade.php
+++ resources/views/livewire/pages/handover-practical-result/index.blade.php
@@ -1,18 +1,38 @@
 <x-container>
     <div class="p-5 space-y-6 bg-white shadow-lg rounded-xl">
         <div class="flex items-center justify-between">
-            <x-text.page-title>
-                Tabel Serah Terima Hasil & Sisa Praktikum
-            </x-text.page-title>
+            <div>
+                <x-text.page-title>
+                    Tabel Serah Terima Hasil & Sisa Praktikum
+                </x-text.page-title>
+
+                {{-- Pilihan Lab di bawah judul --}}
+                <div class="mt-3">
+                    <x-forms.select
+                        class="mt-3 ml-2"
+                        name="selectedLab"
+                        label="Pilih Lab"
+                        wire:model.live='selectedLab'>
+                        @foreach($this->laboratories as $lab)
+                            <option value="{{ $lab->id }}" {{ $lab->id == $selectedLab ? "selected" : "" }}>
+                                {{ $lab->code }} - {{ $lab->name }}
+                            </option>
+                        @endforeach
+                    </x-forms.select>
+                </div>
+            </div>
+
             <div>
                 <a href="{{ route('handover-practical-result.create') }}" wire:navigate>
-                    <x-buttons.fill title="Tambah Kesiapan Bahan Praktikum" color="purple">Buat Serah Terima Hasil & Sisa Praktikum</x-buttons.fill>
+                    <x-buttons.fill title="Tambah Kesiapan Bahan Praktikum" color="purple">
+                        Buat Serah Terima Hasil & Sisa Praktikum
+                    </x-buttons.fill>
                 </a>
             </div>
         </div>
 
         <div>
-            <x-tables.datatable id="tabel-equipmentLoan">
+            <x-tables.datatable id="tabel-practicumHandOver">
                 <thead>
                     <tr>
                         <th># <i class="fa-solid fa-sort ms-2"></i></th>
@@ -22,20 +42,24 @@
                     </tr>
                 </thead>
                 <tbody>
-                    @for ($i = 0; $i < 50; $i++)
-                        <tr>
-                            <td class="font-medium text-gray-900 whitespace-nowrap dark:text-white">{{ $i }}</td>
-                            <td>Matkul - {{ $i }}</td>
-                            <td>Pengampu - {{ $i }}</td>
-                            <td class="flex flex-wrap gap-2 text-center">
-                                <x-badges.outline title="Print" class="px-2.5 py-1.5" color="yellow"><i class="fa-regular fa-print fa-lg"></i></x-badges.outline>
-                                <a href="{{ route('handover-practical-result.edit', ['id' => Crypt::encrypt($i)]) }}" wire:navigate>
-                                    <x-badges.outline title="Ubah" class="px-2.5 py-1.5" color="teal"><i class="fa-regular fa-pen-to-square fa-lg"></i></x-badges.outline>
-                                </a>
-                                <x-badges.outline title="Hapus" class="px-2.5 py-1.5" color="red"><i class="fa-regular fa-trash-can fa-lg"></i></x-badges.outline>
-                            </td>
-                        </tr>
-                    @endfor
+                    <tr>
+                        <td class="font-medium text-gray-900 whitespace-nowrap dark:text-white"></td>
+                        <td></td>
+                        <td></td>
+                        <td class="flex flex-wrap gap-2 text-center">
+                            <x-badges.outline title="Print" class="px-2.5 py-1.5" color="yellow">
+                                <i class="fa-regular fa-print fa-lg"></i>
+                            </x-badges.outline>
+
+                            <x-badges.outline title="Ubah" class="px-2.5 py-1.5" color="teal">
+                                <i class="fa-regular fa-pen-to-square fa-lg"></i>
+                            </x-badges.outline>
+
+                            <x-badges.outline title="Hapus" class="px-2.5 py-1.5" color="red">
+                                <i class="fa-regular fa-trash-can fa-lg"></i>
+                            </x-badges.outline>
+                        </td>
+                    </tr>
                 </tbody>
             </x-tables.datatable>
         </div>
